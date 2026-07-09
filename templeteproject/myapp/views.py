ceb3ca95f8f485d11998b2d6a96ba452771301ac
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-07-09 23:37:08 +0530

    templete project

---
+++ templeteproject/myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+# def home(request):
+#     return render(request,'myapp/home.html')
+
+def website(request):
+    return render(request,'myapp/websit.html')
